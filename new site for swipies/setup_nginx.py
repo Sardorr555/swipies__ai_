4039735ed295451f67981ab5ad0e6873c9bab370
Sardor <albakiev.sardorbek@gmail.com>
2026-09-06 02:03:28 +0500

    fix(deploy): update target server IP to 51.20.190.248 in deploy workflow and nginx configs

--- new site for swipies/setup_nginx.py
+++ new site for swipies/setup_nginx.py
@@ -13,7 +13,7 @@ NGINX_CONF = """\
 server {
     listen 80;
     listen [::]:80;
-    server_name swipies.app www.swipies.app;
+    server_name swipies.app www.swipies.app 51.20.190.248;
 
     # Redirect HTTP to HTTPS
     return 301 https://$host$request_uri;
@@ -22,7 +22,7 @@ server {
 server {
     listen 443 ssl http2;
     listen [::]:443 ssl http2;
-    server_name swipies.app www.swipies.app;
+    server_name swipies.app www.swipies.app 51.20.190.248;
 
     ssl_certificate     /etc/letsencrypt/live/swipies.app/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/swipies.app/privkey.pem;
